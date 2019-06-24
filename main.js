0a2827b34fbd2574af88be07cd29cbd14fe52874
Pieter-Jan Rotsaert <pieterjan.rotsaert@hotmail.com>
2019-06-24 19:32:26 +0200

    Fixed stfu

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ bot.on('message', msg => {
                 msg.channel.send("I'M ALREADY SHUTTING UP YOU FOOL!");
             bobbyEnabled = false;
         }
-        else if(content.indexOf("stfu") || content.indexOf("shut up")){
+        else if(content.indexOf("speak") || content.indexOf("talk")){
             if(!bobbyEnabled)
                 msg.channel.send("FREEDOM!");
             else 
@@ -36,7 +36,7 @@ bot.on('message', msg => {
             msg.channel.send("I do not recognize this command, mortal.");
     }
     else if(content.indexOf("bobby b") !== -1 || content.indexOf("bobbyb") !== -1 || content.indexOf("robert b") !== -1 || content.indexOf("baratheon") !== -1 ||
-            content.indexOf("bobby-b") !== -1){
+            content.indexOf("bobby-b") !== -1 || content.indexOf("bobby") !== -1 ){
         if(bobbyEnabled){
             var idx = Math.floor(Math.random() * globals.BOBBY_QUOTES.length);
             msg.channel.send(globals.BOBBY_QUOTES[idx]);
