1992fd65d929473dd92b636a92b41ee1794f0a95
Pieter-Jan Rotsaert <pieterjan.rotsaert@hotmail.com>
2019-11-28 00:55:21 +0100

    Removed bot token/clientid

--- globals.js
+++ globals.js
@@ -1,7 +1,7 @@
 
 
-const BOT_TOKEN = "NTkxMzQxMTEwMjExOTAzNDg5.XQvafw.TX3_TXwuww4hs7Ma4YjvQzkBuYU"
-const BOT_CLIENTID = "591341110211903489"
+const BOT_TOKEN = process.env.BOT_TOKEN
+const BOT_CLIENTID = process.env.BOT_CLIENTID
 
 const SERVER_ID = "162595925343076352" // MemeNation
 
